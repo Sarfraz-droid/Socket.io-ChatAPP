3dd2563f634d1b1ec3fd7abdeedd7703508cbc2f
Sarfraz-droid <73013838+Sarfraz-droid@users.noreply.github.com>
2021-08-12 02:21:10 +0530

    link

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Form from "./Form";
 import Chat from "./Chat";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:4000");
+const socket = io("https://guarded-scrubland-22840.herokuapp.com/");
 console.log(socket);
 function App() {
 
